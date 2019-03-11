6978aa1915c6207b848b7b25ba819541805a0bef
peachM <1104234512@qq.com>
2019-03-11 09:53:06 +0800

    登录页面及功能实现完成！

--- app.js
+++ app.js
@@ -16,6 +16,22 @@ app.use(cors({
   origin:["http://127.0.0.1:8080","http://localhost:8080"],
   credentials:true
 }))
+//6.1:下载 express-session 并且配置
+const session = require("express-session");
+app.use(session({
+  secret:"128位随机字符", //安全字符串
+  resave:false,          //每次请求是否都更新数据
+  saveUninitialized:true, //初始化时保存数据
+  cookie:{
+    maxAge:1000 * 60 * 60 * 8
+  }
+}));
+//7:加载第三方模块 body-parser
+//body-parser 针对post请求处理请求参数
+//如果配置成功 req.body..
+const bodyParser = require("body-parser");
+//8:配置对特殊 json是否是自动转换 不转换
+app.use(bodyParser.urlencoded({extended:false}))
 
 // 功能一：新书推荐--热门浏览页面/分业
 app.get("/Hotbook",(req,res)=>{
@@ -58,3 +74,32 @@ app.get("/Newbook",(req,res)=>{
     res.send({code:1,data:result});
   })
 })
+
+// 功能四用户登录
+app.get("/Login",(req,res)=>{
+  var uid = req.query.uid;
+  var upwd = req.query.upwd
+  var sql = "SELECT * FROM user WHERE uid = ? AND upwd = ?";
+  pool.query(sql,[uid,upwd],(err,result)=>{
+    if(err) throw err;
+    if(result.length == 0){
+      res.send({code:-1,msg:"学号或密码有误"});
+    }else{
+       //将用户登录凭证保存在服务器端 session对象中
+       var uid = result[0].uid;//获取当前用户id
+       req.session.uid = uid; //保存session
+       res.send({code:1,msg:"登录成功"});
+    }
+  })
+})
+
+// 功能四：点击某一本新书跳转到详情页面
+app.get('/Bookdetail',(req,res)=>{
+  var nid = req.query.nid;
+  var sql = "SELECT * FROM newbook WHERE nid = ?"
+  pool.query(sql,[nid],(err,result)=>{
+    if(err) throw err;
+    // console.log(result);
+    res.send({code:1,data:result});
+  })
+});
